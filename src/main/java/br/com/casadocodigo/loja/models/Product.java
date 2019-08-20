a6b8f29468905ed63ce1e10ad920a6170cde642d
Alan Oliveira <alantechno@gmail.com>
2019-08-19 21:00:44 -0300

    Product prices implementation.

--- src/main/java/br/com/casadocodigo/loja/models/Product.java
+++ src/main/java/br/com/casadocodigo/loja/models/Product.java
@@ -1,5 +1,9 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,22 +20,38 @@ public class Product {
 	private String description;
 	private int pages;
 
+	@ElementCollection
+	private List<Price> prices = new ArrayList<Price>();
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getPages() {
 		return pages;
 	}
+
 	public void setPages(int pages) {
 		this.pages = pages;
+	}
+
+	public List<Price> getPrices() {
+		return prices;
+	}
+
+	public void setPrices(List<Price> prices) {
+		this.prices = prices;
 	}
 }
--- src/main/webapp/WEB-INF/views/products/form.jsp
+++ src/main/webapp/WEB-INF/views/products/form.jsp
@@ -1,6 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
 <meta charset="UTF-8">
@@ -21,6 +20,13 @@
 			<label for="pages">Número de paginas</label> <input type="text"
 				name="pages" id="pages" />
 		</div>
+		<c:forEach items="${bookTypes}" var="bookType" varStatus="status">
+				<div>
+					<label for="preco_${bookType}">${bookType}</label>
+					<input type="text" name="prices[${status.index}].value" id="preco_${bookType}"/>
+					<input type="hidden" name="prices[${status.index}].bookType" value="${bookType}"/>
+				</div>
+			</c:forEach>
 		<div>
 			<input type="submit" value="Enviar">
 		</div>
